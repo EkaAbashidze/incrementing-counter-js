a13b7c2df1506cd985ac7e05eefc7a93bc06750a
Eka <eka.abashidze@gmail.com>
2023-08-28 21:59:07 +0400

    chore: create repo for an old project

---
+++ script.js
@@ -0,0 +1,27 @@
+const counters = document.querySelectorAll(".counter")
+
+counters.forEach(counter => {
+  counter.innerText = "0";
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+
+    const c = +counter.innerText;
+
+    const increment = target / 100;
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`
+      setTimeout (updateCounter, 5);
+    } else {
+      counter.innerText = target;
+    }
+
+    
+
+    console.log(c + increment);
+
+  }
+
+  updateCounter();
+})
